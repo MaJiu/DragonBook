44c570347d95c817844b14ab8ab7bab45ae870f9
MarkJ <mjyzz@foxmail.com>
2020-03-04 09:28:57 +0800

    finish

--- src/Postfix.java
+++ src/Postfix.java
@@ -1,2 +1,9 @@
+import java.io.IOException;
+
 public class Postfix {
+    public static void main(String[] args) throws IOException {
+        Parser parser = new Parser();
+        parser.expr();
+        System.out.write('\n');
+    }
 }
